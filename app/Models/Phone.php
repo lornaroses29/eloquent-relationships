cff71f2aab74b86fe605a488c2e907dbe8337471
lornaroses29 <nazkianr@gmail.com>
2022-11-11 20:33:57 +0700

    first commit

---
+++ app/Models/Phone.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Phone extends Model
+{
+    use HasFactory;
+    
+    /**
+     * user
+     *
+     * @return void
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+/**
+ * @path /users
+ * @method GET
+ */
+Route::get('/users', [\App\Http\Controllers\UserController::class, 'index']);
+
+/**
+ * @path /posts
+ * @method GET
+ */
+Route::get('/posts', [\App\Http\Controllers\PostController::class, 'index']);
